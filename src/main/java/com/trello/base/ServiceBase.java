1bb22edef8719c0b7d919813e4b190fc545844cb
Sahina Kausar <54325901+sahinak@users.noreply.github.com>
2021-04-01 04:04:32 +0530

    Delete src directory

--- src/main/java/com/trello/base/ServiceBase.java
+++
@@ -1,8 +0,0 @@
-package com.trello.base;
-
-import com.trello.utils.CommonUtils;
-import com.trello.utils.GenericRESTHandler;
-
-public class ServiceBase extends CommonUtils {
-    public GenericRESTHandler genRESTHandler = new GenericRESTHandler();
-}
